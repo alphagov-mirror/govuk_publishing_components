ccfcb2f5fc96136a87f195f8f3170590a677fa25
Alex Jurubita <alexandru.jurubita@digital.cabinet-office.gov.uk>
2019-10-10 15:45:36 +0100

    Temp

--- app/assets/config/govuk_publishing_components_manifest.js
+++ app/assets/config/govuk_publishing_components_manifest.js
@@ -1,2 +1,4 @@
+//= link_directory ../javascripts/component_guide .js
 //= link_directory ../javascripts/govuk_publishing_components .js
+//= link_directory ../stylesheets/component_guide .css
 //= link_directory ../stylesheets/govuk_publishing_components .css

37ca03005dd530f7b007796600e721546eea952c
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2020-02-06 15:30:57 +0000

    PROTOTYPE WIP add test route for autocomplete

---
+++ spec/dummy/app/controllers/autocomplete_controller.rb
@@ -0,0 +1,6 @@
+# THIS IS JUST FOR TESTING/PROTOTYPE - delete later!
+class AutocompleteController < ApplicationController
+  def show
+    render 'show', layout: 'dummy_admin_layout'
+  end
+end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   mount GovukPublishingComponents::Engine, at: "/component-guide"
   root to: redirect('/component-guide')
   get 'test', to: 'welcome#index'
+  get 'autocomplete', to: 'autocomplete#show'
   get 'step-nav/:slug', to: 'step_nav#show'
   get 'contextual-navigation', to: 'welcome#contextual_navigation'
   get 'contextual-navigation/*base_path', to: 'welcome#contextual_navigation'
